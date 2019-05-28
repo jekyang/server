4014b36794d901d4b8714e1efff05a46892161ba
Maxence Lange <maxence@artificial-owl.com>
2019-05-28 21:19:58 -0100

    Entities Manager
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>
    some more
    
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>
    fixing stuff, better details
    
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>
    still working on details
    
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>
    details on members
    
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>
    --short option
    
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>
    create entities from command line
    
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>
    cleaner way to check for owner
    
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>
    search within accounts
    
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>
    import displayName during migration
    
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>
    display result in table
    
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>
    admin right, some cleaning and sql logging
    
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>
    add comments to sql log
    
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>
    log exception from sql execution
    
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>
    fixing and cleaning !
    
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>
    fixing
    
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

---
+++ lib/private/Entities/Command/Search.php
@@ -0,0 +1,212 @@
+<?php
+declare(strict_types=1);
+
+
+/**
+ * Nextcloud - Social Support
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Maxence Lange <maxence@artificial-owl.com>
+ * @copyright 2018, Maxence Lange <maxence@artificial-owl.com>
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+namespace OC\Entities\Command;
+
+
+use Exception;
+use OC\Core\Command\Base;
+use OCP\Entities\Helper\IEntitiesHelper;
+use OCP\Entities\IEntitiesManager;
+use OCP\Entities\Implementation\IEntities\IEntities;
+use OCP\Entities\Implementation\IEntitiesAccounts\IEntitiesAccounts;
+use OCP\Entities\Model\IEntityType;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+class Search extends Base {
+
+
+	/** @var IEntitiesManager */
+	private $entitiesManager;
+
+	/** @var IEntitiesHelper */
+	private $entitiesHelper;
+
+	/** @var OutputInterface */
+	private $output;
+
+
+	public function __construct(IEntitiesHelper $entitiesHelper, IEntitiesManager $entitiesManager
+	) {
+		parent::__construct();
+
+		$this->entitiesHelper = $entitiesHelper;
+		$this->entitiesManager = $entitiesManager;
+	}
+
+
+	/**
+	 *
+	 */
+	protected function configure() {
+		parent::configure();
+		$this->setName('entities:search')
+			 ->addArgument('needle', InputArgument::OPTIONAL, 'needle', '')
+			 ->addOption('accounts', '', InputOption::VALUE_NONE, 'search for accounts')
+			 ->addOption('type', '', InputOption::VALUE_REQUIRED, 'limit to a type', '')
+			 ->setDescription('Search for entities');
+	}
+
+
+	/**
+	 * @param InputInterface $input
+	 * @param OutputInterface $output
+	 *
+	 * @throws Exception
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		$output = new ConsoleOutput();
+		$this->output = $output->section();
+
+		$needle = $input->getArgument('needle');
+		$type = $input->getOption('type');
+
+		if ($input->getOption('accounts')) {
+			$this->searchAccounts($needle, $type);
+		} else {
+			$this->searchEntities($needle, $type);
+		}
+		$this->output->writeln('');
+	}
+
+
+	/**
+	 * @param string $needle
+	 * @param string $type
+	 *
+	 * @throws Exception
+	 */
+	private function searchAccounts(string $needle, $type = ''): void {
+
+		if ($type !== '') {
+			$this->verifyType(IEntitiesAccounts::INTERFACE, $type);
+		}
+
+		if ($needle === '') {
+			$accounts = $this->entitiesManager->getAllAccounts($type);
+		} else {
+			$accounts = $this->entitiesManager->searchAccounts($needle, $type);
+		}
+
+		$table = new Table($this->output);
+		$table->setHeaders(['Account Id', 'Type', 'Account', 'Admin']);
+		$table->render();
+		$this->output->writeln('');
+		foreach ($accounts as $account) {
+			$table->appendRow(
+				[
+					'<info>' . $account->getId() . '</info>',
+					'<comment>' . $account->getType() . '</comment>',
+					$account->getAccount(),
+					$account->hasAdminRights() ? '<info>yes</info>' : 'no'
+				]
+			);
+		}
+
+	}
+
+
+	/**
+	 * @param string $needle
+	 * @param string $type
+	 *
+	 * @throws Exception
+	 */
+	private function searchEntities(string $needle, $type = ''): void {
+
+		if ($type !== '') {
+			$this->verifyType(IEntities::INTERFACE, $type);
+		}
+
+		if ($needle === '') {
+			$entities = $this->entitiesManager->getAllEntities($type);
+		} else {
+			$entities = $this->entitiesManager->searchEntities($needle, $type);
+		}
+
+		$table = new Table($this->output);
+		$table->setHeaders(['Entity Id', 'Type', 'Name', 'Owner Id', 'Owner Account', 'Owner Type', 'Admin']);
+		$table->render();
+		$this->output->writeln('');
+		foreach ($entities as $entity) {
+			$ownerId = '';
+			$ownerName = '';
+			$ownerType = '';
+			if ($entity->hasOwner()) {
+				$owner = $entity->getOwner();
+				$ownerId = '<info>' . $owner->getId() . '</info>';
+				$ownerName = $owner->getAccount();
+				$ownerType = '<comment>' . $owner->getType() . '</comment>';
+			}
+
+			$table->appendRow(
+				[
+					'<info>' . $entity->getId() . '</info>',
+					'<comment>' . $entity->getType() . '</comment>',
+					$entity->getName(),
+					$ownerId,
+					$ownerName,
+					$ownerType,
+					$entity->hasAdminRights() ? '<info>yes</info>' : 'no'
+				]
+			);
+		}
+	}
+
+
+	/**
+	 * @param string $interface
+	 * @param string $type
+	 *
+	 * @throws Exception
+	 */
+	private function verifyType(string $interface, string $type) {
+
+		$entityTypes = $this->entitiesHelper->getEntityTypes($interface);
+		$types = array_map(
+			function(IEntityType $item) {
+				return $item->getType();
+			}, $entityTypes
+		);
+
+		if (!in_array($type, $types)) {
+			throw new Exception('Please specify a type: ' . implode(', ', $types));
+		}
+
+	}
+
+}
+
